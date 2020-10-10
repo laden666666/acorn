9d99e09dcecee45cfd22f0d50bf3a3cd5b4e7e6a
jingzhi.njz <jingzhi.njz@antfin.com>
2020-10-10 17:09:07 +0800

    增加一个例子 增加 loose 模式的约定

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
-const {parse} = require('./acorn/src');
+// const {parse} = require('./acorn/src');
+const {parse} = require('./acorn-loose/dist/acorn-loose');
 
-console.log(parse('1*1', {}))
+console.log(JSON.stringify(parse(`var a = 1;
+var 
+var c = 3;`, {}), null, 4));
